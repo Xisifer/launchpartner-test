09dbef6dea346af420b3312eb3ea653067880afd
Xisifer <xisifer@gmail.com>
2020-03-24 21:55:53 -0500

    Bootstrapped some input fields for name, description and portrait. Not hooked up to anything yet.

--- src/components/imageUpload.js
+++ src/components/imageUpload.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-// import logo from './logo.svg';
+import {Form} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class ImageUpload extends Component {
 
@@ -18,11 +19,27 @@ class ImageUpload extends Component {
 
   render() {
     return (
-        <div className="App">
-        <input type="file" />
-        <p>Hello, world</p>
-        <button onClick={this.uploadFile}>Upload</button>
-        </div>
+        // <div className="App">
+        // <input type="file" />
+        // <p>Hello, world</p>
+        
+        // </div>
+        <Form id="profile-submission">
+            <Form.Label>Upload a portrait here:</Form.Label>
+            <Form.File 
+                id="custom-file"
+                label="Custom file input"
+                custom
+            />
+            <br />
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control type="text" placeholder="Name" />
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Tell us about yourself!</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+            <button onClick={this.uploadDesc}>Upload Profile</button>
+        </Form>
     );
   }
 }
